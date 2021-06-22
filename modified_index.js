f8bd6e32e483a9fb4dd2d5c4b1117a7b04ee63f2
PreethiKalyani99 <68461351+PreethiKalyani99@users.noreply.github.com>
2021-06-22 17:39:47 +0530

    Create modified_index.js

---
+++ modified_index.js
@@ -0,0 +1,15 @@
+const githubSite = require("./github_site");
+
+console.log("Start of Site Status check");
+
+githubSite.getMostRecentMaintenanceStartDate().then(function(value){
+    console.log(value);
+    return githubSite.siteStatus();
+}).then(function(res){
+    console.log(res);
+}).then(function () {
+    console.log("The end");
+});
+    
+
+
